6e826a8ba905e9da8e6307a1080fd99a4fb7ca56
Wayne Sun <waynesun95@gmail.com>
2016-12-04 23:07:25 -0500

    Made list adapter, next step is to rewrite for actual data from login screen and then make onClickListener

--- app/src/main/java/com/android/application/carmaintenanceapp/FirstScreen.java
+++ app/src/main/java/com/android/application/carmaintenanceapp/FirstScreen.java
@@ -2,14 +2,14 @@ package com.android.application.carmaintenanceapp;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.facebook.login.LoginManager;
@@ -17,17 +17,74 @@ import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
 
 public class FirstScreen extends AppCompatActivity {
 
-    public class MyAdapter extends BaseAdapter {
+    private ArrayList<replaceWithCarClass> information_array;
+    private listAdapter information_array_adapter;
+    private ListView carList;
+
+    public class replaceWithPersonClass {
+        private ArrayList<replaceWithCarClass> carList;
+
+        public replaceWithPersonClass(ArrayList<replaceWithCarClass> carList) {
+            this.carList = carList;
+        }
+
+        public void addCar(String carName, String estimatedVal, String expenses) {
+            replaceWithCarClass newCar = new replaceWithCarClass(carName, estimatedVal, expenses);
+            this.carList.add(newCar);
+        }
+    }
+
+    public class replaceWithCarClass {
+        private String carName;
+        private String estimatedVal;
+        private String expenses;
+
+        public replaceWithCarClass(String carName, String estimatedVal, String expenses) {
+            this.carName = carName;
+            this.estimatedVal = estimatedVal;
+            this.expenses = expenses;
+        }
+    }
+
+    public class listAdapter extends ArrayAdapter<replaceWithCarClass> {
+        public listAdapter(Context context, ArrayList<replaceWithCarClass> listItems) {
+            super(context, 0, listItems);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            replaceWithCarClass car = getItem(position);
+
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.car_list_layout, parent, false);
+            }
+
+            TextView car_name = (TextView) convertView.findViewById(R.id.car_name);
+            TextView car_value = (TextView) convertView.findViewById(R.id.car_value);
+            TextView car_expenses = (TextView) convertView.findViewById(R.id.car_expenses);
+
+            car_name.setText(car.carName);
+            car_value.setText(car.estimatedVal);
+            car_expenses.setText(car.expenses);
+
+            return convertView;
+        }
+
+    }
+
+    /*public class MyAdapter extends BaseAdapter {
 
         private Context context;
-        private String[] texts = {"car name TEST TEXT", "estimated value TEST TEXT", "expenses TEST TEXT" };
+        private ArrayList<String> texts;
 
-        public MyAdapter(Context context) {
+        public MyAdapter(Context context, ArrayList<String> array) {
             this.context = context;
+            texts = array;
         }
 
         public int getCount() {
@@ -35,7 +92,8 @@ public class FirstScreen extends AppCompatActivity {
         }
 
         public Object getItem(int position) {
-            return texts[position];
+            //return texts[position];
+            return texts.get(position);
         }
 
         public long getItemId(int position) {
@@ -47,17 +105,18 @@ public class FirstScreen extends AppCompatActivity {
 
             if (convertView == null) {
                 tv = new TextView(context);
-                tv.setLayoutParams(new GridView.LayoutParams(85, 85));
+                tv.setLayoutParams(new GridView.LayoutParams(150, 150));
             }
 
             else {
                 tv = (TextView) convertView;
             }
 
-            tv.setText(texts[position]);
+            //tv.setText(texts[position]);
+            tv.setText(texts.get(position));
             return tv;
         }
-    }
+    }*/
 
     String TAG = "First Screen";
 
@@ -66,10 +125,34 @@ public class FirstScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen);
 
+
+        carList = (ListView) findViewById(R.id.carList);
+        //INSTEAD OF MAKING A NEW CAR ARRAY HERE, WE WILL LOAD IT IN FROM INTENT DATA
+        replaceWithCarClass carItem = new replaceWithCarClass("Tesla", "$1000", "$100000");
+
+        //array of data (Car name, estimated value, expenses)
+        information_array = new ArrayList<replaceWithCarClass>();
+        information_array.add(carItem);
+
+        information_array_adapter = new listAdapter(this, information_array);
+        carList.setAdapter(information_array_adapter);
+        information_array_adapter.notifyDataSetChanged();
+
+        /*
         //inflate GridView
         GridView gridView = (GridView) findViewById(R.id.carList);
-        gridView.setAdapter(new MyAdapter(this));
+        information_array_adapter = new MyAdapter(this, information_array);
+        gridView.setAdapter(information_array_adapter);
+        */
     }
+
+    //TEST FUNCTION TO POPULATE LIST
+    public void addCar(View view) {
+        replaceWithCarClass testCar = new replaceWithCarClass("test car", "$232", "$1524");
+        information_array.add(testCar);
+        information_array_adapter.notifyDataSetChanged();
+    }
+
 
     public void onBackPressed() {
 
